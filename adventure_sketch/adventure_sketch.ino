81b456e5b61be7eaaf4adfbf5984e7a1dc65b559
ajpetrosky <petroskyandrew@gmail.com>
2017-04-04 14:17:51 -0400

    Set up the pins and size of the LCD screen

--- adventure_sketch/adventure_sketch.ino
+++ adventure_sketch/adventure_sketch.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(20, 4);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
